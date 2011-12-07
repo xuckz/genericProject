855107b0b54792390b10b32e25abf50b96c4e58f
thegameterrorist <normanfreak@gmail.com>
2011-12-07 21:45:08 +0100

    - added new block type GRASS

--- genericproject/Block.java
+++ genericproject/Block.java
@@ -17,14 +17,16 @@ public class Block {
 	final static int TYPE_SAND = 2;
 	final static int TYPE_WOOD = 3;
 	final static int TYPE_LEAF = 4;
+	final static int TYPE_GRASS = 5;
 	
 	final static float[] COLOR_DIRT = {0.82f, 0.48f, 0.18f};
 	final static float[] COLOR_ROCK = {0.67f, 0.63f, 0.60f};
 	final static float[] COLOR_SAND = {0.96f, 0.93f, 0.22f};
 	final static float[] COLOR_WOOD = {0.69f, 0.41f, 0.01f};
 	final static float[] COLOR_LEAF = {0.53f, 0.91f, 0.28f};
+	final static float[] COLOR_GRASS = {0.43f, 0.81f, 0.28f};
 	
-	final static float[][] COLOR_MAP = {COLOR_DIRT, COLOR_ROCK, COLOR_SAND, COLOR_WOOD, COLOR_LEAF};
+	final static float[][] COLOR_MAP = {COLOR_DIRT, COLOR_ROCK, COLOR_SAND, COLOR_WOOD, COLOR_LEAF, COLOR_GRASS};
 	
 	public Block(float x, float y, float z, int type)
 	{
--- genericproject/BlockWorld.java
+++ genericproject/BlockWorld.java
@@ -26,6 +26,8 @@ public class BlockWorld {
 		blocks.add(new Block(1, 0, 2, Block.TYPE_SAND));
 		blocks.add(new Block(1, 0, 1, Block.TYPE_SAND));
 		blocks.add(new Block(1, 0, 0, Block.TYPE_DIRT));
+		blocks.add(new Block(-1, 0, 0, Block.TYPE_GRASS));
+		blocks.add(new Block(-1, 0, 1, Block.TYPE_GRASS));
 		
 	}
 	
